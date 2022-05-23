9942c6fedc33f39f4edcefe3169719105d3fc8bf
Denesis Peralta <dcolmenares1901@gmail.com>
2022-05-23 18:31:53 +0200

    add 1.4

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const unordenlist = document.createElement("ul");
 for (let index = 0; index < countries.length; index++) {
   const contentList = document.createElement("li");
   unordenlist.appendChild(contentList);
-  const textContent = document.createTextNode(countries[index]);
-  contentList.appendChild(textContent);
+  const textContent1 = document.createTextNode(countries[index]);
+  contentList.appendChild(textContent1);
 }
 
 document.body.append(unordenlist);
@@ -30,25 +30,39 @@ divBody.appendChild(unordenlist1);
 
 for (let index = 0; index < cars.length; index++) {
   const contentList1 = document.createElement("li");
+  // const textContent1 = document.createTextNode(cars[index]);
+  // contentList1.appendChild(textContent1);
+  contentList1.textContent = cars[index];
   unordenlist1.appendChild(contentList1);
-  const textContent1 = document.createTextNode(cars[index]);
-  contentList1.appendChild(textContent1);
 }
 
 document.body.append(unordenlist);
 
 // 1.4 Crea dinamicamente en el html una lista de div que contenga un elemento
 // h4 para el titulo y otro elemento img para la imagen.
-// const countries = [
-// 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'},
-// 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
-// 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
-// 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
-// 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
-// ];
+const countries1 = [
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=1" },
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=2" },
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=3" },
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=4" },
+  { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=5" },
+];
+
+const contentDiv = (source) => {
+  for (const content of source) {
+    const divContent = document.createElement("div");
+    const titleH4 = document.createElement("h4");
+    const titleImg = document.createElement("img");
+    titleH4.textContent = content.title;
+    titleImg.src = content.imgUrl;
+    divContent.append(titleH4, titleImg);
+    document.body.append(divContent);
+  }
+};
 
 // 1.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último
 // elemento de la lista.
+const boton = document.querySelector("#removeUlt");
 
 // 1.6 Basandote en el ejercicio anterior. Crea un botón para cada uno de los
 // elementos de las listas que elimine ese mismo elemento del html.
